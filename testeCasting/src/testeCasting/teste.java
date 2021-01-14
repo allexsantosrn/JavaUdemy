316c27677993d05be4182e514aa641555fd76b1f
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2021-01-13 23:26:24 -0300

    Update projects 01 e 02

---
+++ testeCasting/src/testeCasting/teste.java
@@ -0,0 +1,22 @@
+package testeCasting;
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		//Declaração das variáveis
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		//Execução do Casting
+		resultado = (double) a / b;
+		
+		//Impressão do resultado
+		System.out.println(resultado);
+
+	}
+
+}
